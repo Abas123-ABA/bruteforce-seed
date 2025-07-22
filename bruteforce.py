884c6ae14aaf6a2e69fbda69afadb069bfb127f1
Abas123-ABA <mahmoudmouhmed139@gmail.com>
2025-07-22 14:46:34 -0400

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -1,7 +1,7 @@
 # https://github.com/mcdallas/cryptotools
 
 import sys
-from bip_utils import Bip39MnemonicValidator
+from bip_utils import 
 from cryptotools.BTC import Xprv
 import argparse
 
@@ -14,7 +14,7 @@ def find_correct_seed(words, address_to_find, address_type):
 			test_array = words.split()
 			test_array[i] = word_to_try
 			test = " ".join(test_array)
-			if not Bip39MnemonicValidator(test).Validate():
+			if not5KTGL3GhKP1bw4mePbdbgHJsRBtMJLb8yj9gw9FDV6cA5bAfhis(test).Validate():
 				continue
 			m = Xprv.from_mnemonic(test)
 			m.encode()
